d0b9bbd8ecc9800f3f5847e0b588bb39a48c194f
xuandai-py <buinguyenxuandai@gmail.com>
2021-08-12 01:22:45 +0700

    initialize - config - add models

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './guest/home/home.component';
+import { LoginComponent } from './guest/login/login.component';
+import { RegisterComponent } from './guest/register/register.component';
+import { ProfileComponent } from './user/profile/profile.component';
+import { NotFoundComponent } from './error/not-found/not-found.component';
+
+import { UnauthorizedComponent } from './error/unauthorized/unauthorized.component';
+import { AdminComponent } from './admin/admin/admin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    ProfileComponent,
+    NotFoundComponent,
+ 
+    UnauthorizedComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
